455c2babd96685fbf2da22bbf81742f7b0b89b73
Teelirium <angryzombi01@gmail.com>
2023-01-20 12:37:11 +0500

    add the funny

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,49 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as path from "path";
+import * as vscode from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Extension "main-emoji" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "main" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+//   let disposable = vscode.commands.registerCommand("main.hi", () => {
+//     vscode.window.showInformationMessage("hi");
+//   });
+//   context.subscriptions.push(disposable);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('main.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Main!');
-	});
+  const hoverDisposable = vscode.languages.registerHoverProvider(
+    { pattern: "**/*" },
+    {
+      provideHover(document, position) {
+        const content = new vscode.MarkdownString(
+          '<img src="main.png" alt=":main:" height="128" width="128">'
+        );
+        content.isTrusted = true;
+        content.supportHtml = true;
+        content.supportThemeIcons = true;
+        content.baseUri = vscode.Uri.file(
+          path.join(context.extensionPath, "src", "images", path.sep)
+        );
 
-	context.subscriptions.push(disposable);
+        const targetText = document.getText(
+          document.getWordRangeAtPosition(position)
+        );
+        // vscode.window.showInformationMessage(text);
+
+        if (targetText.toLocaleLowerCase() !== "main") {
+          return;
+        }
+
+        return new vscode.Hover(content, new vscode.Range(position, position));
+      },
+    }
+  );
+  context.subscriptions.push(hoverDisposable);
 }
 
 // This method is called when your extension is deactivated
